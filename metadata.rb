28ded07c4da154bac61042455c995d1f252d31e4
Matthew Ahrenstein <matt@ahrenstein.com>
2015-05-10 18:05:05 -0400

    Updating metadata to have more info

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,13 @@ maintainer_email '@ahrenstein'
 license          'See LICENSE.txt'
 description      'Performs various tweaks on fresh Linux installs'
 long_description 'Removes Landscape garbage from Ubuntu systems, tweaks system-wide bashrc settings'
-version          '0.2.0'
+version          '0.2.1'
 depends          'line', '0.6.1'
+
+%w{ ubuntu debian centos }.each do |os|
+  supports os
+end
+
+recipe 'linux-tweak::default', 'Call this recipe to run the various tweaks from the cookbook'
+recipe 'linux-tweak::bashrc', 'Configure system-wide bashrc preferences so bash looks nice. Also adds some useful aliases'
+recipe 'linux-tweak::packages', 'Adds some useful packages that aren\'t installed by default and removes landscape from Ubuntu systems'
